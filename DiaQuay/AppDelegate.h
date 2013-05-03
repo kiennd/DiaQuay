3f9030fdaa53327a559e8e72ef6d8b590eead33c
kiennd <kien.nguyenduc08@gmail.com>
2013-05-03 07:31:35 +0700

    Initial Commit

---
+++ DiaQuay/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  DiaQuay
+//
+//  Created by KIENND on 5/3/13.
+//  Copyright (c) 2013 KIENND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ DiaQuay/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DiaQuay
+//
+//  Created by KIENND on 5/3/13.
+//  Copyright (c) 2013 KIENND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
